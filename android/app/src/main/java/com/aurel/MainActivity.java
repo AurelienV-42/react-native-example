e647a680f524fef3b687df8353db0682706ac9e7
AurelienV <aurelienvpro@gmail.com>
2023-01-09 00:37:54 +0100

    [+] SplashScreen on Android

--- android/app/src/main/java/com/aurel/MainActivity.java
+++ android/app/src/main/java/com/aurel/MainActivity.java
@@ -3,6 +3,8 @@ package com.aurel;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen; // Import this.
+import android.os.Bundle; // Import this.
 
 public class MainActivity extends ReactActivity {
 
@@ -14,6 +16,12 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "aurel";
   }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
